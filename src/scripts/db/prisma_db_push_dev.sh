1530ea04ec2128963d3e85ff4fdda27aeb2b0155
Gabriel <bbynogueira@gmail.com>
2023-03-05 18:01:40 -0300

    Added scripts to prisma db push

---
+++ src/scripts/db/prisma_db_push_dev.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+if [[ $branch == dev ]]
+then dotenv -e .env.dev -- npx prisma db push
+else echo \"CHANGE BRANCH TO DEV!\"
+fi;
---
+++ src/scripts/db/prisma_db_push_prod.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+if [[ $branch == main ]]
+then dotenv -e .env.prod -- npx prisma db push
+else echo \"CHANGE BRANCH TO DEV!\"
+fi;
