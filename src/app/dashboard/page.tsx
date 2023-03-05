5bb4996555408a728fb3a987c92579357d405c4a
Gabriel <bbynogueira@gmail.com>
2023-03-05 10:37:18 -0300

    loggnig servernev

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,21 @@
 import NewGameButton from '@components/NewGameButton';
-import usePresenceChannels from '@hooks/usePresenceChannels';
 import Container from '@ui-components/Container';
 import Link from 'next/link';
 
 export default async function Dashboard() {
-  const presenceChannels = await usePresenceChannels();
+  // const presenceChannels = await usePresenceChannels();
 
   return (
     <Container>
       <h1>Dashboard Page</h1>
       <NewGameButton> Subscribe to a channel</NewGameButton>
-      {presenceChannels.map((channel) => {
+      {/* {presenceChannels.map((channel) => {
         return channel ? (
           <div className={'w-50 h-3'}>
             {`${channel.name} ${channel.userCount}`}
           </div>
         ) : null;
-      })}
+      })} */}
       <Link href='/'>&larr; Go Back</Link>
     </Container>
   );
--- src/env/server.mjs
+++ src/env/server.mjs
@@ -3,8 +3,10 @@
  * This file is included in `/next.config.mjs` which ensures the app isn't built with invalid env vars.
  * It has to be a `.mjs`-file to be imported there.
  */
-import { serverSchema, _serverEnv } from './schema.mjs';
 import { clientEnv, formatErrors } from './client.mjs';
+import { serverSchema, _serverEnv } from './schema.mjs';
+
+console.log('_serverEnv', _serverEnv);
 
 const __serverEnv = serverSchema.safeParse(_serverEnv);
 
