0761fd5e07243b3262968aad3ed9537dfdff6df7
Gabriel <bbynogueira@gmail.com>
2023-03-05 17:51:04 -0300

    created script to migrate dev

---
+++ src/scripts/db/prisma_migrate_prod.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+if [[ $branch == main ]]
+then dotenv -e .env.prod -- npx prisma migrate dev
+else echo \"CHANGE BRANCH TO MAIN!\"
+fi;
